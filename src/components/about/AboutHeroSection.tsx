7e7464fe0ae900aeb3731156785c10f492c86a8f
Susit <sujitsubedi567@gmail.com>
2026-06-02 13:13:37 +0545

    about

--- src/components/about/AboutHeroSection.tsx
+++ src/components/about/AboutHeroSection.tsx
@@ -18,29 +18,36 @@ export default function AboutHeroSection() {
       <div className="relative z-10 flex flex-col min-h-screen">
         <Navbar />
 
-        <Image
-          src="/about/rectangle.svg"
-          alt="Hero Car"
-          width={310}
-          height={150}
-          className="mt-8 absolute top-45 -left-10 z-0 hidden lg:block"
-        />
-
-        <Image
-          src="/about/rectangle.svg"
-          alt="Hero Car"
-          width={200}
-          height={150}
-          className="mt-8 absolute top-26 -left-15 z-0 block lg:hidden"
-        />
-
         <div className="flex flex-1 items-center max-w-7xl mx-auto w-full lg:px-6 lg:px-8 pl-4 pb-25  lg:py-0 z-10">
-          <div className="max-w-[520px]">
-            <h1 className="font-sora font-extrabold text-black leading-tight text-4xl md:text-5xl lg:text-6xl">
-              <span className="px-2 rounded-sm ">About</span> Us
+          <div className="max-w-[520px] z-10 relative">
+            <Image
+              src="/about/rectangle.svg"
+              alt="Hero Car"
+              width={310}
+              height={150}
+              className="mt-8 absolute -top-8  -left-28 z-0 hidden lg:block"
+            />
+            <Image
+              src="/about/rectangle.svg"
+              alt="Hero Car"
+              width={200}
+              height={150}
+              className="mt-8 absolute -top-8  -left-18 z-0 block md:hidden lg:hidden"
+            />
+
+            <Image
+              src="/about/rectangle.svg"
+              alt="Hero Car"
+              width={290}
+              height={150}
+              className="mt-8 absolute  -top-8 -left-30 z-0  hidden md:block lg:hidden"
+            />
+
+            <h1 className="relative font-sora font-extrabold text-black leading-tight text-4xl md:text-5xl lg:text-6xl z-10">
+              <span className="px-2 rounded-sm z-10">About</span> Us
             </h1>
 
-            <p className="lg:mt-6 mt-2 text-[#000000] text-[16px]  lg:leading-relaxed font-medium font-poppins max-w-[240px] lg:max-w-[490px]">
+            <p className="lg:mt-6 mt-2 text-[#000000] text-[16px]  lg:leading-relaxed font-medium font-poppins max-w-[240px] md:max-w-[390px] lg:max-w-[490px]">
               Making travel across Nepal simpler, safer, and more reliable
               through smart vehicle booking. From work trips to family travel,
               vacations, or daily rides, Popular Ride connects you with trusted
